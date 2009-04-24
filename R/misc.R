3d89b533a4805c277d54cc544d1b3d535cc29be6
Dan Kelley <Dan.Kelley@Dal.Ca>
2009-04-24 00:00:00 +0000

    version 0.1.0

---
+++ R/misc.R
@@ -0,0 +1,19 @@
+check.tokens <- function(tokens, expected)
+{
+    nt <- length(tokens)
+    ne <- length(expected)
+    if (nt != ne) stop("wrong number of words on line; got", nt, "but need", ne)
+    for (i in 1:nt) {
+        if (tokens[i] != expected[i]) stop("expecting word", expected[i], "but got", tokens[i])
+    }
+}
+trim.whitespace <- function(x)
+{
+    n <- length(x)
+    for (i in 1:n) {
+        x[i] <- gsub("^[ \t]*", "", x[i])
+        x[i] <- gsub("[ \t]*$", "", x[i])
+    }
+    x
+}
+
